39d08db23bcaae1e5d15b572ce257c384bd62525
tanaka <lemo.nade.engineer.room@gmail.com>
2024-06-07 19:47:40 +0900

    add AppleScript Language

--- src/lib/scripts/language.ts
+++ src/lib/scripts/language.ts
@@ -1,15 +1,26 @@
 import type { LanguageFn } from 'highlight.js';
+import applescript from 'highlight.js/lib/languages/applescript';
 
 /**
  * Syntax Highlighting Code Language
  */
 export class Language {
-
 	/**
 	 * @param name The name of the language
 	 * @param fn The function that highlights the code
 	 * @param extensions The file extensions that the language supports
 	 */
-	constructor(readonly name: string, readonly fn: LanguageFn, readonly extensions: readonly string[]) {}
-}
+	constructor(
+		readonly name: string,
+		readonly fn: LanguageFn,
+		readonly extensions: readonly string[]
+	) {}
 
+	static readonly appleScript = new Language('applescript', applescript, [
+		'.applescript',
+		'.scpt',
+		'.scptd'
+	]);
+
+	static readonly allCases: readonly Language[] = [this.appleScript];
+}
