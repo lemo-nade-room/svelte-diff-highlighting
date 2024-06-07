fddbc3c6af70e97d70e21b55abadc199015a94b4
tanaka <lemo.nade.engineer.room@gmail.com>
2024-06-07 20:49:18 +0900

    add reset css and import web-font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import './reset.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
+
+	main {
+		font-family: 'Assistant', sans-serif;
+		font-weight: normal;
+	}
+</style>
