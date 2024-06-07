db60fbea139ef7f07e69c85c2b47bdf8b09c9ec6
tanaka <lemo.nade.engineer.room@gmail.com>
2024-06-07 22:22:36 +0900

    Refactor import and type usage in hljs.ts
    Refactored the hljs.ts file to improve typecasting and imports. Swapped the used HLJS import from a library type to specific HLJSApi and AutoHighlightResult types, ensuring clearer and more precise type definition. This enhances type safety and code clarity.

--- src/lib/scripts/hljs.ts
+++ src/lib/scripts/hljs.ts
@@ -1,14 +1,17 @@
-import hljsLibrary, { type AutoHighlightResult } from 'highlight.js';
+import { type AutoHighlightResult, type HLJSApi } from 'highlight.js';
 import type { Language } from '$lib/scripts/language.js';
-type Hljs = typeof hljsLibrary;
 
-export function hljsRegisterLanguages(hljs: Hljs, languages: readonly Language[]) {
+export function hljsRegisterLanguages(hljs: HLJSApi, languages: readonly Language[]) {
 	for (const language of languages) {
 		hljs.registerLanguage(language.name, language.fn);
 	}
 }
 
-export function highlightText(hljs: Hljs, text: string, language?: Language): AutoHighlightResult {
+export function highlightText(
+	hljs: HLJSApi,
+	text: string,
+	language?: Language
+): AutoHighlightResult {
 	if (language === undefined) {
 		return hljs.highlightAuto(text);
 	}
