0917842315906b2adbc8446ddb677867d85d2dbb
tanaka <lemo.nade.engineer.room@gmail.com>
2024-06-08 21:23:19 +0900

    Add RemovedMarkSection to display updated code differences
    A new RemovedMarkSection has been created in Svelte to show changes in code. It visually presents old and new code side by side, highlighting the altered sections. This update also includes the addition of the RemovedMarkSection to the routes file to make it accessible to users.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import SyntaxHighlightSection from '$sample/sections/syntaxHighlight/SyntaxHighlightSection.svelte';
 	import AdditionalMarkSection from '$sample/sections/additionalMark/AdditionalMarkSection.svelte';
+	import RemovedMarkSection from '$sample/sections/removedMark/RemovedMarkSection.svelte';
 </script>
 
-<AdditionalMarkSection></AdditionalMarkSection>
-<SyntaxHighlightSection></SyntaxHighlightSection>
+<RemovedMarkSection />
+<AdditionalMarkSection />
+<SyntaxHighlightSection />
---
+++ src/sample/sections/removedMark/RemovedMarkSection.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+	import Highlighted from '$lib/ui/Highlighted.svelte';
+	import { Language } from '$lib/scripts/language.js';
+	import 'highlight.js/styles/xcode.css';
+
+	const newText = `func fizzbuzz(number: Int) -> String {
+	if number % 3 == 0 {
+		return "Fizz"
+	}
+	if number % 5 == 0 {
+		return "Buzz"
+	}
+	return String(number)
+}`;
+	const oldText = `func fizzbuzz(number: Int) -> String {
+	if number % 15 == 0 {
+		return "FizzBuzz"
+	}
+	if number % 3 == 0 {
+		return "Fizz"
+	}
+	if number % 5 == 0 {
+		return "Buzz"
+	}
+	return String(number)
+}`;
+</script>
+
+<section class="additional-mark-section">
+	<h2>Removed Code Diff</h2>
+	<div class="targets">
+		<div class="old target">
+			<h3>Old Code</h3>
+			<div class="code">
+				<Highlighted setNumber text={oldText} language={Language.swift} />
+			</div>
+		</div>
+		<div class="new target">
+			<h3>New Code</h3>
+			<div class="code">
+				<Highlighted setNumber text={newText} language={Language.swift} />
+			</div>
+		</div>
+	</div>
+
+	<div class="diff">
+		<h3>Difference</h3>
+		<Highlighted setNumber text={newText} language={Language.swift} old={oldText} />
+	</div>
+</section>
+
+<style>
+	.additional-mark-section {
+		width: 100svw;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		& > .targets {
+			width: 90svw;
+			display: flex;
+			align-items: flex-start;
+
+			& > .target {
+				width: 40svw;
+				& > h3 {
+					width: 100%;
+					font-size: 18px;
+					font-weight: 600;
+					margin-bottom: 12px;
+				}
+
+				font-family: var(--code-font-family), monospace;
+				font-size: 14px;
+				font-optical-sizing: auto;
+				font-weight: 400;
+				font-style: normal;
+				box-shadow: var(--box-shadow);
+				padding: 12px 24px;
+				border-radius: 4px;
+			}
+
+			& > .new {
+				margin-left: 24px;
+			}
+		}
+
+		& > .diff {
+			& > h3 {
+				width: 100%;
+				font-size: 18px;
+				font-weight: 600;
+				margin-bottom: 12px;
+			}
+			margin-top: 24px;
+			font-family: var(--code-font-family), monospace;
+			font-size: 14px;
+			font-optical-sizing: auto;
+			font-weight: 400;
+			font-style: normal;
+			width: 480px;
+			max-width: 90svw;
+			box-shadow: var(--box-shadow);
+			padding: 12px 24px;
+		}
+	}
+</style>
