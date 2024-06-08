d87c9da23de4fabfaffd553f6fb3c473a068af39
tanaka <lemo.nade.engineer.room@gmail.com>
2024-06-08 21:15:16 +0900

    Enhance styling for Highlighted.svelte
    The styling for .line and .line-number classes in Highlighted.svelte has been enhanced. The enhancement includes modifications to height, position, color, font properties, and margins for .line-number element. Also for .added class, a border is added, using pseudo-element :before, for distinct visibility.

--- src/lib/ui/Highlighted.svelte
+++ src/lib/ui/Highlighted.svelte
@@ -69,17 +69,36 @@
 
 			& > .line {
 				min-width: 100%;
+				height: 100%;
 				display: block;
 				box-sizing: content-box;
 				line-height: var(--line-height, 1.4);
+				position: relative;
 
-					& > .line-number {
-							display: inline-block;
-							text-align: var(--line-number-text-align, right);
-					}
+				& > .line-number {
+					display: inline-block;
+					text-align: var(--line-number-text-align, right);
+					color: var(--line-number-color, #6e6e73ff);
+					font-weight: var(--line-number-font-weight, normal);
+					font-size: var(--line-number-font-size, 14px);
+					font-family: var(--line-number-font-family), monospace;
+					margin-right: var(--line-number-margin-right, 1em);
+					margin-left: var(--line-number-margin-left, 0.5em);
+				}
 
 				&.added {
 					background-color: var(--added-background, #eaf3ffff);
+
+					&:before {
+						content: '';
+						position: absolute;
+						top: 0;
+						left: 0;
+						background-color: var(--added-marker-border-color, #4ca9ffff);
+						display: inline-block;
+						width: var(--added-marker-border-width, 4px);
+						height: 100%;
+					}
 				}
 			}
 		}
