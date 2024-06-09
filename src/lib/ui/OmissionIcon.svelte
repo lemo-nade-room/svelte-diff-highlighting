7776727cdeeeb9ab8ee7bd45654f8e1006bf9faa
tanaka <lemo.nade.engineer.room@gmail.com>
2024-06-09 12:20:22 +0900

    Update omission icon styling and animation
    This commit includes enhancements to the omission icon design and functionality. It modifies the icon to use flex display for better center alignment and smooth transition effect. Additionally, it introduces a new color variable and refines the icon's shape and background color with clip-path and border-color properties.

--- src/lib/ui/OmissionIcon.svelte
+++ src/lib/ui/OmissionIcon.svelte
@@ -12,8 +12,7 @@
 
 <style>
 	.omission-icon {
-		display: block;
-		background-color: var(--background-color, #ffb74c);
+		background-color: var(--omission-icon-border-color, white);
 		--length: var(--size, 1em);
 		--height: calc(var(--length) / 2 * tan(60deg));
 		height: var(--height);
@@ -22,7 +21,18 @@
 		position: relative;
 		transform: rotate(90deg);
 		transform-origin: center calc(var(--height) / 3 * 2);
-		transition: transform 0.1s;
+		transition: transform 0.3s;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		&:before {
+			content: '';
+			display: block;
+			width: calc(100% - 2px);
+			height: calc(100% - 2px);
+			clip-path: polygon(50% 0, 100% 100%, 0 100%);
+			background-color: var(--omission-icon-background-color, #ffb74c);
+		}
 
 		&.open {
 			transform: rotate(180deg);
