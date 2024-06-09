a68fe2f133cdaf4423aa40a9339471fa19f9f853
tanaka <lemo.nade.engineer.room@gmail.com>
2024-06-09 20:26:20 +0900

    Add PropertyList and PropertyItem components
    Created the PropertyList and PropertyItem components to display property details. Introduced a PropertyItem type for property information in propertyItem.ts. These components are imported and used in the +page.svelte in the api routes.

---
+++ src/routes/api/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import BorderedSection from '$sample/components/section/BorderedSection.svelte';
+	import SectionTitleText from '$sample/components/text/SectionTitleText.svelte';
+	import PropertyList from '$sample/components/propertyList/PropertyList.svelte';
+	import type { PropertyItem } from '$sample/components/propertyList/propertyItem.js';
+
+	const properties: readonly PropertyItem[] = [
+		{
+			name: 'text',
+			type: 'string',
+			required: false,
+			default: "''",
+			description: 'The string to be syntax highlighted'
+		},
+		{
+			name: 'old',
+			type: 'string',
+			required: false,
+			default: 'undefined',
+			description: 'Used only when a diff display is needed.'
+		},
+		{
+			name: 'languages',
+			type: 'readonly Language[]',
+			required: false,
+			default: 'Language.allCases',
+			description:
+				'List of languages registered with Highlight.js. If using a language not available in this library, create a custom Language instance and combine it with Language.allCases in the property'
+		},
+		{
+			name: 'language',
+			type: 'Language',
+			required: false,
+			default: 'undefined',
+			description:
+				'The language to be highlighted by Highlight.js. Specify a language included in languages. If not specified, the language of the text will be automatically determined.'
+		},
+		{
+			name: 'setNumber',
+			type: 'boolean',
+			required: false,
+			default: 'false',
+			description: 'Whether to display line numbers or not'
+		}
+	];
+</script>
+
+<BorderedSection>
+	<SectionTitleText>Highlighted</SectionTitleText>
+	<h3>Svelte Properties</h3>
+	<PropertyList {properties} />
+</BorderedSection>
---
+++ src/sample/components/propertyList/PropertyItem.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import type { PropertyItem } from '$sample/components/propertyList/propertyItem.js';
+
+	export let item: PropertyItem;
+</script>
+
+<li class="property-item">
+	<h4 class="name">{item.name}</h4>
+	<p class="attribute">
+		<span class="type">{item.type}</span>
+		<span class="is-option">{item.required ? 'Required' : 'Optional'}</span>
+		{#if !item.required}
+			<span class="default">default: {item.default}</span>
+		{/if}
+	</p>
+	<p class="description">{item.description}</p>
+</li>
+
+<style>
+	.property-item {
+		padding: 12px 0;
+		& > .name {
+			font-weight: bold;
+			font-size: 18px;
+			margin-bottom: 8px;
+		}
+		& > .attribute {
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: flex-start;
+			font-size: 14px;
+			margin-bottom: 8px;
+			margin-left: 12px;
+			& > .type {
+				display: block;
+				padding: 0 8px;
+				width: fit-content;
+				font-weight: bold;
+				font-size: 14px;
+				color: white;
+				background-color: #b1b1b3;
+				border-radius: 5px;
+			}
+		}
+		& > .description {
+			font-size: 18px;
+			margin-left: 12px;
+		}
+	}
+</style>
